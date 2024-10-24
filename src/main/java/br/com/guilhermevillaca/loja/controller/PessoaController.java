d39de91375d9f5a89b9fc23b56c87872e3cdcc14
Guilherme Villaca <guidvillaca@gmail.com>
2024-10-24 18:27:28 -0300

    atualizado controllers pessoa e usuario

--- src/main/java/br/com/guilhermevillaca/loja/controller/PessoaController.java
+++ src/main/java/br/com/guilhermevillaca/loja/controller/PessoaController.java
@@ -1,15 +1,19 @@
 package br.com.guilhermevillaca.loja.controller;
 
 import br.com.guilhermevillaca.loja.model.Pessoa;
+import br.com.guilhermevillaca.loja.model.Pessoa;
 import br.com.guilhermevillaca.loja.repository.PessoaRepository;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Optional;
 /**
  *
  * @author villaca
@@ -20,11 +24,55 @@ public class PessoaController {
     
     @Autowired
     PessoaRepository pessoaRepository;
+    
     @RequestMapping(value = "listar", method = RequestMethod.GET)
     public ResponseEntity<List<Pessoa>> get() {
         List<Pessoa> pessoas = new ArrayList<>();
         pessoas = (List<Pessoa>) pessoaRepository.findAll();
         return ResponseEntity.ok(pessoas);
+    }
+
+    @RequestMapping(value = "listar/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Pessoa> getById(@PathVariable(value = "id") Integer id) {
+        Optional<Pessoa> pessoa = pessoaRepository.findById(id);
+        if (pessoa.isPresent()) {
+            return new ResponseEntity<Pessoa>(pessoa.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    //post
+    @RequestMapping(value = "novo", method = RequestMethod.POST)
+    public Pessoa novo(@RequestBody Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
+
+    //put
+    @RequestMapping(value = "atualizar/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Pessoa> atualizar(@PathVariable(value = "id") Integer id, @RequestBody Pessoa newPessoa) {
+        Optional<Pessoa> oldPessoa = pessoaRepository.findById(id);
+        if (oldPessoa.isPresent()) {
+            Pessoa pessoa = oldPessoa.get();
+            pessoa.setNome(newPessoa.getNome());
+            pessoaRepository.save(pessoa);
+            return new ResponseEntity<Pessoa>(pessoa, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //delete
+    @RequestMapping(value = "remover/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Pessoa> delete(@PathVariable(value = "id") Integer id) {
+        Optional<Pessoa> pessoa = pessoaRepository.findById(id);
+        if (pessoa.isPresent()) {
+            pessoaRepository.delete(pessoa.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/br/com/guilhermevillaca/loja/controller/UsuarioController.java
+++ src/main/java/br/com/guilhermevillaca/loja/controller/UsuarioController.java
@@ -4,14 +4,19 @@
  */
 package br.com.guilhermevillaca.loja.controller;
 
+import br.com.guilhermevillaca.loja.model.Produto;
 import br.com.guilhermevillaca.loja.model.Usuario;
 import br.com.guilhermevillaca.loja.repository.UsuarioRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 /**
  *
@@ -23,9 +28,55 @@ public class UsuarioController {
     
     @Autowired
     UsuarioRepository usuarioRepository;
+
     @RequestMapping(value = "listar", method = RequestMethod.GET)
     public ResponseEntity<List<Usuario>> get() {
         return ResponseEntity.ok((List<Usuario>) usuarioRepository.findAll());
+    }
+
+    @RequestMapping(value = "listar/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Usuario> getById(@PathVariable(value = "id") Integer id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isPresent()) {
+            return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    //post
+    @RequestMapping(value = "novo", method = RequestMethod.POST)
+    public Usuario novo(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    //put
+    @RequestMapping(value = "atualizar/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Usuario> atualizar(@PathVariable(value = "id") Integer id, @RequestBody Usuario newUsuario) {
+        Optional<Usuario> oldUsuario = usuarioRepository.findById(id);
+        if (oldUsuario.isPresent()) {
+            Usuario usuario = oldUsuario.get();
+            usuario.setLogin(newUsuario.getLogin());
+            usuario.setPessoa(newUsuario.getPessoa());
+            usuario.setSenha(newUsuario.getSenha());
+            usuarioRepository.save(usuario);
+            return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //delete
+    @RequestMapping(value = "remover/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Usuario> delete(@PathVariable(value = "id") Integer id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isPresent()) {
+            usuarioRepository.delete(usuario.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
